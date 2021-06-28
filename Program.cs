de6256f4f92fb099280d4924c9d5e53b2e7e9c78
José M. Alarcón <jmalarcon@campusmvp.es>
2021-06-28 10:01:08 +0200

    v1.0

---
+++ Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindNullifiedFiles
+{
+    class Program
+    {
+        private static int numFolders = 0;
+        private static int numFiles = 0;
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0 || (args[0] == "-?") || (args[0] == "/?"))
+            {
+                ShowHelp();
+                return;
+            }
+
+            string path = Path.GetFullPath(args[0]);
+            if (!Directory.Exists(path))
+            {
+                Console.WriteLine("Folder \"{0}\" does not exist. Exiting...", path);
+                return;
+            }
+
+            int numDaysAgo = 30;
+            int numNulls = 10;
+            if (args.Length > 1)
+            {
+                _ = int.TryParse(args[1], out numDaysAgo);
+                if (numDaysAgo == 0) numDaysAgo = 30;
+            }
+            if (args.Length > 2)
+            {
+                _ = int.TryParse(args[2], out numNulls);
+                if (numNulls == 0) numNulls = 10;
+            }
+
+            Console.WriteLine("Finding nullified files...");
+            Stopwatch sw = Stopwatch.StartNew();
+            Processfolder(path, DateTime.Now.AddDays(-numDaysAgo), numNulls);
+            sw.Stop();
+            Console.WriteLine("{0}: {1} nullified files found in {2} folders in {3}",
+                              DateTime.Now,
+                              numFiles,
+                              numFolders,
+                              sw.Elapsed);
+        }
+
+        private static void ShowHelp()
+        {
+            Console.WriteLine("Finds all the files in a folder that have at least the first 10 bytes as null and have been modified 30 days ago or less.");
+            Console.WriteLine("Usage: FindNullifiedFiles folder [max number of days the files have been modified(30)] [num of nulls at the beginning of file (10)]");
+            Console.WriteLine("Ej: FindNullifiedFiles C:\\MyFolder");
+            Console.WriteLine("Ej: FindNullifiedFiles C:\\MyFolder 15 30");
+            Console.WriteLine("(c) José M. Alarcón [www.jasoft.org]");
+        }
+
+        private static void Processfolder(string path, DateTime fromDate, int numNulls)
+        {
+            numFolders++;
+            DirectoryInfo di = new DirectoryInfo(path);
+            FileInfo[] files = di.GetFiles().Where(file => file.LastWriteTime >= fromDate).ToArray<FileInfo>();
+            foreach (FileInfo file in files)
+            {
+                try
+                {
+                    if (IsNullifiedFile(file.FullName, numNulls, file.Length))
+                    {
+                        numFiles++;
+                        Console.WriteLine(file.FullName);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error reading file {0}: {1}", file.FullName, ex.Message);
+                }
+            }
+
+            //Ahora recorremos los subdirectorios pertinentes
+            DirectoryInfo[] folders = di.GetDirectories();
+
+            foreach (DirectoryInfo carpeta in folders)
+            {
+                Processfolder(carpeta.FullName, fromDate, numNulls);
+            }
+
+        }
+
+        private static bool IsNullifiedFile(string filePath, int minNumNullsToFind, long fileLength)
+        {
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException();
+
+            FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            BinaryReader sr = new BinaryReader(fs);
+            //Check if it's made of nulls
+            int numOfNullsRead = 0;
+            while ( numOfNullsRead < minNumNullsToFind && numOfNullsRead < fileLength && sr.ReadByte() == (byte)0)
+            {
+                numOfNullsRead++;
+            }
+            sr.Close();
+            //If all nulls, is a nullified file
+            return numOfNullsRead == minNumNullsToFind || numOfNullsRead == fileLength;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # FindNullifiedFiles
-Finds files consisting in all nulls in an specified folder
+
+![FindNullifiedFiles icon](FindNullifiedFiles_250.png)
+
+This is a quick&dirty small utility I wrote to find all the files having only nulls that are inside an specified folder (or subfolders).
+
+I needed this to find a lot of files a cloud drive I was using broke, filling them with nulls.
+
+It's written with .NET 5 and will work in Windows, Linux and Mac, although I've only compiled it for Windows (just open the project and compile it for your desired operating system).
+
+## Usage
+
+```bash
+FindNullifiedFiles PathToFolder [Max Age of files (30)] [Number of butes to inspect (10)]
+```
+
+By default, it will search for files with a maximum age of **30 days**, and instead of reading the full file contents, for the sake of speed, it will read by default only **the first 10 bytes** to check them for *nulls*. You can change both parameters from the command line:
+
+```bash
+FindNullifiedFiles "C:\My Files" 90 30 > MyNullFiles.txt
+```
+
+This will search all the files inside the `C:\My Files` folder and subfolders, with a maximum age of 30 days that have a `null` in the first 30 bytes  and send the results to a .txt file in the current folder. You can then use the text file to process the files with Microsoft Excel or any other system you want.
+
+Please note that the program shows some information in the first ("Beginning...") and last lines (when the search was done and how many nullified files it has found) that you may want to remove.
+
+If there's some kind of error reading a file you'll see a line that starts with `"Error reading file..."`, so you can check it manually.
+
+Hope this helps!
